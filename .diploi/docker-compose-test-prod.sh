6176cacc0af2de6299237b317cfc715ec6b2811c
Marcus Ahlfors <marcus.ahlfors@nemesys.fi>
2025-03-20 23:41:38 +0200

    Docker compose test scripts

---
+++ .diploi/docker-compose-test-prod.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# Script to test run the component in docker compose
+#
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+cd ..
+
+
+rm -rf component-test
+mkdir -p component-test/component
+rsync -av --progress --exclude component-test  --exclude .diploi ./ component-test/component
+docker compose --project-directory component-test --file .diploi/docker-compose-prod.yml  --progress plain up --build
